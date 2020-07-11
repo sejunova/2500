1e4612b624c8e401fab73d029816e60f55bfc929
sejun <sejun@quryon.com>
2020-07-12 01:47:46 +0900

    [Ass3]

--- Assignment3/src/academy/pocu/comp2500/assignment3/AttackIntent.java
+++ Assignment3/src/academy/pocu/comp2500/assignment3/AttackIntent.java
@@ -14,7 +14,13 @@ public class AttackIntent {
                 if (x >= 0 && x < 16 && y >= 0 && y < 8) {
                     // 피해치(x, y) = (공격 지점에서의 피해치) * (1 - 공격 지점으로부터의 거리 / (공격의 AoE 값 + 1))
                     IntVector2D targetArea = new IntVector2D(x, y);
-                    int targetDist = area.getDistance(targetArea);
+                    int targetDist;
+                    if (area.equals(targetArea)) {
+                        targetDist = 0;
+                    } else {
+                        targetDist = Math.max(Math.abs(area.getX() - targetArea.getX()), Math.abs(area.getY() - targetArea.getY()));
+                    }
+
                     int ap = (int) (attackUnit.getAp() * (1 - targetDist / (double) (aoe + 1)));
                     this.attackInfos.add(new AttackInfo(targetArea, ap));
                 }
