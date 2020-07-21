e3dbe6b72d4e30e9fc2e6d9dd1aa8742b0d17ebb
sejun <sejun@quryon.com>
2020-07-21 11:10:12 +0900

    [Lab10]

--- Lab10/src/academy/pocu/comp2500/lab10/ResultValidator.java
+++ Lab10/src/academy/pocu/comp2500/lab10/ResultValidator.java
@@ -1,5 +1,7 @@
 package academy.pocu.comp2500.lab10;
 
+import academy.pocu.comp2500.lab10.pocuflix.NotFoundResult;
+import academy.pocu.comp2500.lab10.pocuflix.OkResult;
 import academy.pocu.comp2500.lab10.pocuflix.ResultBase;
 import academy.pocu.comp2500.lab10.pocuflix.ResultCode;
 
@@ -11,6 +13,26 @@ public class ResultValidator {
     }
 
     public boolean isValid(ResultCode code) {
-        return this.result.getCode().equals(code);
+        boolean typeCheck;
+        switch (code) {
+            case OK:
+                typeCheck = this.result instanceof OkResult;
+                break;
+            case NOT_FOUND:
+                typeCheck = this.result instanceof NotFoundResult;
+                break;
+            case NOT_MODIFIED:
+                typeCheck = this.result instanceof CachedResult;
+                break;
+            case UNAUTHORIZED:
+                typeCheck = this.result instanceof UnauthorizedResult;
+                break;
+            case SERVICE_UNAVAILABLE:
+                typeCheck = this.result instanceof ServiceUnavailableResult;
+                break;
+            default:
+                throw new RuntimeException("Invalid Result Class!!");
+        }
+        return this.result.getCode().equals(code) && typeCheck;
     }
 }
