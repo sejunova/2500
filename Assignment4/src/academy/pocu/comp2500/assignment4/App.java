fcce45f2f14008898c3a29b29ebef964ebab67a4
sejun <sejun@quryon.com>
2020-07-22 16:09:20 +0900

    [Ass4] 코드포맷

--- Assignment4/src/academy/pocu/comp2500/assignment4/App.java
+++ Assignment4/src/academy/pocu/comp2500/assignment4/App.java
@@ -4,7 +4,7 @@ import academy.pocu.comp2500.assignment4.registry.Registry;
 
 public class App {
     public App(Registry registry) {
-         registry.registerDrawPixelCommandCreator("DrawPixelCommand");
+        registry.registerDrawPixelCommandCreator("DrawPixelCommand");
         registry.registerIncreasePixelCommandCreator("IncreasePixelCommand");
         registry.registerDecreasePixelCommandCreator("DecreasePixelCommand");
         registry.registerToUppercaseCommandCreator("ToUppercaseCommand");
