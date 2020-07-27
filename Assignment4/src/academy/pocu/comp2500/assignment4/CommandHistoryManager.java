3807419550008b864f79eaf8ed3f46902750005d
sejun <sejun@quryon.com>
2020-07-28 00:29:03 +0900

    [Ass4] can Redo Undo 사용

--- Assignment4/src/academy/pocu/comp2500/assignment4/CommandHistoryManager.java
+++ Assignment4/src/academy/pocu/comp2500/assignment4/CommandHistoryManager.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class CommandHistoryManager {
     private Canvas canvas;
     private ArrayList<ICommand> commands = new ArrayList<>();
-    private int curCommandIndex;
+    private int curCommandIndex = -2;
 
     public CommandHistoryManager(Canvas canvas) {
         this.canvas = canvas;
@@ -32,6 +32,9 @@ public class CommandHistoryManager {
     }
 
     public boolean undo() {
+        if (!canUndo()) {
+            return false;
+        }
         boolean isExecuted = this.commands.get(this.curCommandIndex).undo();
         if (isExecuted) {
             this.curCommandIndex--;
@@ -40,6 +43,9 @@ public class CommandHistoryManager {
     }
 
     public boolean redo() {
+        if (!canRedo()) {
+            return false;
+        }
         boolean isExecuted = this.commands.get(this.curCommandIndex + 1).redo();
         if (isExecuted) {
             this.curCommandIndex++;
